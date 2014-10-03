842de274e77cfc2c3c72fdeb8a6a04fb3fbceab0
Michael Yu <ItzWarty@gmail.com>
2014-10-02 22:59:53 -0700

    NMockitoInstance cleanup, InitializeMocks clears previous interactions.
    It seems visual studio unit tests don't reset the state of past unit tests.

--- NMockitoInstance.cs
+++ NMockitoInstance.cs
@@ -6,18 +6,15 @@ namespace NMockito
 {
    public class NMockitoInstance
    {
-      public void InitializeMocks() { NMockitoAttributes.InitializeMocks(this); }
+      public void InitializeMocks() { ClearInteractions(); NMockitoAttributes.InitializeMocks(this); }
 
       public T CreateMock<T>() where T : class { return NMockitoStatic.CreateMock<T>(); }
       public object CreateMock(Type type) { return NMockitoStatic.CreateMock(type); }
 
-      public WhenContext<T> When<T>(T value)
-      {
-         return new WhenContext<T>();
-      }
-
-      public INMockitoTimesMatcher Times(int count) { return new NMockitoTimesEqualMatcher(count); }
       public INMockitoTimesMatcher AnyTimes() { return new NMockitoTimesAnyMatcher(); }
+      public INMockitoTimesMatcher Times(int count) { return new NMockitoTimesEqualMatcher(count); }
+
+      public WhenContext<T> When<T>(T value) { return new WhenContext<T>(); }
 
       public T Verify<T>(T mock, INMockitoTimesMatcher times = null) where T : class { return NMockitoStatic.Verify(mock, times); }
 
@@ -29,20 +26,11 @@ namespace NMockito
       public void ClearInteractions<T>(T mock, int expectedCount) { NMockitoStatic.ClearInteractions(mock, expectedCount); }
 
       #region Assertions
-      [DebuggerHidden]
-      public void AssertEquals<T>(T expected, T actual) { Assert.AreEqual(expected, actual); }
-
-      [DebuggerHidden]
-      public void AssertNull<T>(T value) { Assert.IsNull(value); }
-
-      [DebuggerHidden]
-      public void AssertNotNull<T>(T value) { Assert.IsNotNull(value); }
-
-      [DebuggerHidden]
-      public void AssertTrue(bool value) { Assert.IsTrue(value); }
-
-      [DebuggerHidden]
-      public void AssertFalse(bool value) { Assert.IsFalse(value); }
+      [DebuggerHidden] public void AssertEquals<T>(T expected, T actual) { Assert.AreEqual(expected, actual); }
+      [DebuggerHidden] public void AssertNull<T>(T value) { Assert.IsNull(value); }
+      [DebuggerHidden] public void AssertNotNull<T>(T value) { Assert.IsNotNull(value); }
+      [DebuggerHidden] public void AssertTrue(bool value) { Assert.IsTrue(value); }
+      [DebuggerHidden] public void AssertFalse(bool value) { Assert.IsFalse(value); }
       #endregion
 
       #region Smart Parameters
