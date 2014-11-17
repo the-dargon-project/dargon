7d417b023723b48c489f8758991f3c3939a70e6a
Michael Yu <ItzWarty@gmail.com>
2014-11-16 19:52:36 -0800

    Properly handle NMockitoEquals testing for null.

--- NMockitoEquals.cs
+++ NMockitoEquals.cs
@@ -4,7 +4,7 @@
    {
       private readonly object value;
       public NMockitoEquals(object value) { this.value = value; }
-      public bool Test(object value) { return this.value.Equals(value); }
+      public bool Test(object value) { return object.Equals(this.value, value); }
       public override string ToString() { return "[Equals " + value + "]"; }
       public override bool Equals(object obj) { return obj is NMockitoEquals && ((NMockitoEquals)obj).value == this.value; }
    }
