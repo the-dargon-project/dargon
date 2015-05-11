5b767b082674f31b6a4dc675699831a5b40aa9b2
Michael Yu <ItzWarty@gmail.com>
2015-05-11 12:54:22 -0700

    Support mocking singleton-backed static classes.
    The implementation adds a StaticType attribute to MockAttribute. When
    our mocking framework's MockAttribute is used with the static type,
    we look at the field the MockAttribute is applied to and hope the static
    type has an instance of that type.

--- NMockitoAttributes.cs
+++ NMockitoAttributes.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Linq;
 using System.Reflection;
+using Castle.DynamicProxy.Internal;
 
-namespace NMockito
-{
+namespace NMockito {
    public static class NMockitoAttributes
    {
       public static void InitializeMocks(object target)
@@ -13,8 +12,24 @@ namespace NMockito
          foreach (var field in fields) {
             var mockAttribute = field.GetCustomAttribute<MockAttribute>();
             if (mockAttribute != null) {
-               var fieldType = field.FieldType;
-               field.SetValue(target, NMockitoStatic.CreateMock(fieldType, mockAttribute.Tracked));
+               InitializeMockedField(target, field, mockAttribute);
+            }
+         }
+      }
+
+      private static void InitializeMockedField(object target, FieldInfo mockField, MockAttribute mockAttribute) {
+         var fieldType = mockField.FieldType;
+         var mock = NMockitoStatic.CreateMock(fieldType, mockAttribute.Tracked);
+         mockField.SetValue(target, mock);
+
+         var staticType = mockAttribute.StaticType;
+         if (staticType != null) {
+            var staticFields = staticType.GetFields(BindingFlags.NonPublic | BindingFlags.Static);
+            var staticInstanceFields = staticFields.Where(field => field.Name.EndsWith("instance") && field.FieldType == fieldType).ToArray();
+            if (staticInstanceFields.Length != 1) {
+               throw new AmbiguousMatchException("NMockito unable to determine instance field of static " + staticType.FullName + " candidates " + string.Join(", ", staticInstanceFields.Select(f=>f.Name).ToArray()));
+            } else {
+               staticInstanceFields.First().SetValue(null, mock);
             }
          }
       }
