387dcde71e0a349ea1ef2c3d62c441aa19625a82
Warty <ItzWarty@gmail.com>
2018-07-30 00:37:13 -0700

    [Commons] Restore Traverse.Dfs/Bfs/Dive recursive descent methods.
    I have no clue what to call Dive. I really hate that name.
    
    These helper methods make it real clean to traverse recursive structures
    in a functional/linq-friendly manner. For example, a tree might be
    searched as follows:
    
       root.Bfs(n => n.Child).First(n => n.Name == "Fred")
    
    Of note, an older variant from ItzWarty.Commons supported various
    additional features:
    
       1. including/excluding root via flag
       2. a termination condition alongside the child getter func
       3. accumulation alongside descended tree path.
    
    [1] is most useful with [3]. Of note, 3 allows traversing a tree
    to select the full names of paths (i.e. root/asdf/jkl) and 1 complements
    this by excluding the root from these accumulations (that is, its
    descendents are the seeds, so that paths like asdf/jkl are returned rather
    than /asdf/jkl).
    
    These were most useful in FS-intensive Dargon. As I'm not really doing
    that stuff anymore, I've omitted the APIs for now. This yields the crazy
    clean Dfs/Bfs/Dive funcs which are certainly higher-alloc and less
    flexible.

---
+++ framework/src/Dargon.Commons/src/Dargon/Commons/Traverse.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dargon.Commons {
+   public static class Traverse {
+      public static IEnumerable<T> Dfs<T>(this T root, Func<T, IEnumerable<T>> next) {
+         var s = new Stack<T>();
+         s.Push(root);
+
+         var temp = new Stack<T>();
+
+         while (s.Count != 0) {
+            var n = s.Pop();
+            yield return n;
+
+            foreach (var c in next(n)) temp.Push(c);
+            while (temp.Count != 0) s.Push(temp.Pop());
+         }
+      }
+
+      public static IEnumerable<T> Bfs<T>(this T root, Func<T, IEnumerable<T>> next) {
+         var q = new Queue<T>();
+         q.Enqueue(root);
+
+         while (q.Count != 0) {
+            var n = q.Dequeue();
+            yield return n;
+
+            foreach (var c in next(n)) q.Enqueue(c);
+         }
+      }
+
+      public static IEnumerable<T> Dive<T>(this T root, Func<T, T> next) where T : class {
+         while (root != null) {
+            yield return root;
+            root = next(root);
+         }
+      }
+   }
+}
