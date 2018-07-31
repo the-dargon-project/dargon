637763c3cfff277b77544c91c7abcb0f94667e4c
Warty <ItzWarty@gmail.com>
2018-07-31 03:31:04 -0700

    [DMI] Support REPL embed in other applications.
    Simply extract Main's command registration to a helper method which
    enhances a REPL dispatcher. Other applications can embed DMI's behavior by
    enhancing their REPL dispatchers with this method.

---
+++ tools/src/dargon-management-interface/src/Dargon/Courier/Management/Repl/DargonManagementInterfaceReplStatics.cs
@@ -0,0 +1,32 @@
+﻿using Dargon.Repl;
+
+namespace Dargon.Courier.Management.Repl {
+   public static class DargonManagementInterfaceReplStatics {
+      public static DispatcherCommand RegisterDargonManagementInterfaceCommands(this DispatcherCommand dispatcher) {
+         dispatcher.RegisterCommand(new UseCommand());
+         dispatcher.RegisterCommand(new FetchMobsCommand());
+         dispatcher.RegisterCommand(new FetchOperationsCommand());
+         dispatcher.RegisterCommand(new ChangeDirectoryCommand());
+         dispatcher.RegisterCommand(new ListDirectoryCommand());
+         dispatcher.RegisterCommand(new InvokeCommand());
+         dispatcher.RegisterCommand(new SetCommand());
+         dispatcher.RegisterCommand(new GetCommand());
+         dispatcher.RegisterCommand(new GraphCommand());
+         dispatcher.RegisterCommand(new TreeCommand());
+         dispatcher.RegisterCommand(new ExitCommand());
+         dispatcher.RegisterCommand(new DispatcherMultiCommand(
+            "a",
+            new[] { "use tcp 127.0.0.1:21337", "fetch-mobs", "cd !!UdpDebugMob", "fetch-ops", "ls" },
+            dispatcher));
+         dispatcher.RegisterCommand(new DispatcherMultiCommand(
+            "aa",
+            new[] { "use tcp 127.0.0.1:21338", "fetch-mobs", "cd !!UdpDebugMob", "fetch-ops", "ls" },
+            dispatcher));
+         dispatcher.RegisterCommand(new DispatcherMultiCommand(
+            "aaa",
+            new[] { "use tcp 127.0.0.1:21339", "fetch-mobs", "cd !!UdpDebugMob", "fetch-ops", "ls" },
+            dispatcher));
+         return dispatcher;
+      }
+   }
+}
--- tools/src/dargon-management-interface/src/Dargon/Courier/Management/Repl/DmiEntryPoint.cs
+++ tools/src/dargon-management-interface/src/Dargon/Courier/Management/Repl/DmiEntryPoint.cs
@@ -6,38 +6,17 @@ using NLog.Targets;
 using NLog.Targets.Wrappers;
 
 namespace Dargon.Courier.Management.Repl {
-   public class Program {
+   public static class DmiEntryPoint {
       public static void Main() {
          InitializeLogging();
 
          var dispatcher = new DispatcherCommand("root");
-         dispatcher.RegisterCommand(new UseCommand());
-         dispatcher.RegisterCommand(new FetchMobsCommand());
-         dispatcher.RegisterCommand(new FetchOperationsCommand());
-         dispatcher.RegisterCommand(new ChangeDirectoryCommand());
-         dispatcher.RegisterCommand(new ListDirectoryCommand());
-         dispatcher.RegisterCommand(new InvokeCommand());
-         dispatcher.RegisterCommand(new SetCommand());
-         dispatcher.RegisterCommand(new GetCommand());
-         dispatcher.RegisterCommand(new GraphCommand());
-         dispatcher.RegisterCommand(new TreeCommand());
-         dispatcher.RegisterCommand(new ExitCommand());
-         dispatcher.RegisterCommand(new DispatcherMultiCommand(
-            "a",
-            new[] { "use tcp 127.0.0.1:21337", "fetch-mobs", "cd !!UdpDebugMob", "fetch-ops", "ls" },
-            dispatcher));
-         dispatcher.RegisterCommand(new DispatcherMultiCommand(
-            "aa",
-            new[] { "use tcp 127.0.0.1:21338", "fetch-mobs", "cd !!UdpDebugMob", "fetch-ops", "ls" },
-            dispatcher));
-         dispatcher.RegisterCommand(new DispatcherMultiCommand(
-            "aaa",
-            new[] { "use tcp 127.0.0.1:21339", "fetch-mobs", "cd !!UdpDebugMob", "fetch-ops", "ls" },
-            dispatcher));
+         dispatcher.RegisterDargonManagementInterfaceCommands();
          new ReplCore(dispatcher).Run();
       }
 
-      private static void InitializeLogging() {
+
+      public static void InitializeLogging() {
          var config = new LoggingConfiguration();
          Target debuggerTarget = new DebuggerTarget() {
             Layout = "${longdate}|${level}|${logger}|${message} ${exception:format=tostring}"
