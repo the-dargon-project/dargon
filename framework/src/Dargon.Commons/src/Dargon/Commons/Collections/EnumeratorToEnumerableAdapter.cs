0bf16ed0b25b222c6fec846b5630cd157389b0c4
Warty <ItzWarty@gmail.com>
2018-08-17 15:22:40 -0700

    [Commons] Add EnumeratorToEnumerableAdapter
    C#'s foreach relies on a duck-typed ienumerable which implements method
    GetEnumerator(). This allows types like List<T> to return a struct from
    GetEnumerator() to avoid a GC alloc.
    
    This return value (even if a struct) must implement IEnumerator<T>. The
    enumerator object is akin to an iterator, while the IEnumerable-like
    (with GetEnumerator) is akin to a collection view.
    
    A read-only collection view can be represented by the initial state of an
    iterator. C# provides no such adapter for this. To serve this purpose,
    I've written this helper class which exposes an iterator as a
    IEnumerable-like collection view.

---
+++ framework/src/Dargon.Commons/src/Dargon/Commons/Collections/EnumeratorToEnumerableAdapter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Dargon.Commons.Collections {
+   public struct EnumeratorToEnumerableAdapter<TItem, TEnumerator> where TEnumerator : struct, IEnumerator<TItem> {
+      private readonly TEnumerator enumerable;
+
+      public EnumeratorToEnumerableAdapter(TEnumerator enumerable) {
+         this.enumerable = enumerable;
+      }
+
+      public TEnumerator GetEnumerator() => enumerable;
+   }
+
+   public static class EnumeratorToEnumerableAdapter<TItem> {
+      public static EnumeratorToEnumerableAdapter<TItem, TEnumerable> Create<TEnumerable>(TEnumerable enumerable) where TEnumerable : struct, IEnumerator<TItem> {
+         return new EnumeratorToEnumerableAdapter<TItem, TEnumerable>(enumerable);
+      }
+   }
+}
