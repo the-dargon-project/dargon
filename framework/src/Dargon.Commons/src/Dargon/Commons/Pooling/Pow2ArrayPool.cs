2c55b36ee99af45ed77b4d0dd8d767404870ec54
Warty <ItzWarty@gmail.com>
2020-01-05 11:13:58 -0800

    [commons] Pow2ArrayPool to Pow2CollectionPool for swappable collection type.

--- framework/src/Dargon.Commons/src/Dargon/Commons/Pooling/Pow2ArrayPool.cs
+++ framework/src/Dargon.Commons/src/Dargon/Commons/Pooling/Pow2ArrayPool.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Dargon.Commons.Pooling {
-   public class Pow2ArrayPool<T> {
-      private readonly Func<int, IObjectPool<T[]>> poolFactory;
-      private readonly IObjectPool<T[]>[] pools;
+   public abstract class Pow2CollectionPoolBase<TCollection> {
+      private readonly Func<int, IObjectPool<TCollection>> poolFactory;
+      private readonly IObjectPool<TCollection>[] pools;
 
-      public Pow2ArrayPool(string name, Func<int, IObjectPool<T[]>> poolFactory) {
+      public Pow2CollectionPoolBase(string name, Func<int, IObjectPool<TCollection>> poolFactory) {
          Name = name;
          this.poolFactory = poolFactory;
          pools = Arrays.Create(32, i => poolFactory(i == 0 ? 0 : 1 << (i - 1)));
@@ -29,15 +30,29 @@ namespace Dargon.Commons.Pooling {
          return (bin, actualSize);
       }
 
-      public T[] Take(int minimumSize) {
+      public TCollection Take(int minimumSize) {
          var (bin, actualSize) = FindBinAndActualSize(minimumSize);
          return pools[bin].TakeObject();
       }
 
-      public void Return(T[] arr) {
-         var (bin, actualSize) = FindBinAndActualSize(arr.Length);
-         if (actualSize != arr.Length) throw new InvalidOperationException($"Array length mismatch. Actual {arr.Length} vs expected {actualSize}.");
-         pools[bin].ReturnObject(arr);
+      public void Return(TCollection collection) {
+         var (bin, actualSize) = FindBinAndActualSize(Capacity(collection));
+         if (actualSize != Capacity(collection)) throw new InvalidOperationException($"Collection length mismatch. Actual {Capacity(collection)} vs expected {actualSize}.");
+         pools[bin].ReturnObject(collection);
       }
+
+      protected abstract int Capacity(TCollection collection);
+   }
+
+   public class Pow2ArrayPool<T> : Pow2CollectionPoolBase<T[]> {
+      public Pow2ArrayPool(string name, Func<int, IObjectPool<T[]>> poolFactory) : base(name, poolFactory) { }
+
+      protected override int Capacity(T[] collection) => collection.Length;
+   }
+
+   public class Pow2ListPool<T> : Pow2CollectionPoolBase<List<T>> {
+      public Pow2ListPool(string name, Func<int, IObjectPool<List<T>>> poolFactory) : base(name, poolFactory) { }
+
+      protected override int Capacity(List<T> collection) => collection.Count;
    }
 }
