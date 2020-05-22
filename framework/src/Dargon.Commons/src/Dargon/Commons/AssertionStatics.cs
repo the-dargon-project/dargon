10e737043245851734247fcf1f82e4c829628246
Michael Yu <miyu@dargon.io>
2020-05-22 05:17:03 -0700

    [commons] AssertionStatics for fluent assertions

---
+++ framework/src/Dargon.Commons/src/Dargon/Commons/AssertionStatics.cs
@@ -0,0 +1,13 @@
+﻿namespace Dargon.Commons {
+   public static class AssertionStatics {
+      public static bool AssertIsTrue(this bool val, string message = null) {
+         Assert.IsTrue(val, message);
+         return val;
+      }
+
+      public static bool AssertIsFalse(this bool val, string message = null) {
+         Assert.IsTrue(val, message);
+         return val;
+      }
+   }
+}
