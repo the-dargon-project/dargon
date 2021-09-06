8b3911670e335b2504972abde7a77358b0ae788d
Michael Yu <miyu@dargon.io>
2021-09-05 21:05:38 -0700

    [commons] LazyStatics for Lazy<int> x = Lazy(() => 1 + 2)

---
+++ framework/src/Dargon.Commons/src/Dargon/Commons/LazyStatics.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dargon.Commons {
+   public static class LazyStatics {
+      public static Lazy<T> Lazy<T>(Func<T> func) => new(func);
+   }
+}
