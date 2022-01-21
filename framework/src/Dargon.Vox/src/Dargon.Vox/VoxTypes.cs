b4568367faa9058c7ece10c7e1d6c1885e4d0f6a
Michael Yu <miyu@dargon.io>
2022-01-21 04:14:01 -0800

    [vox] VoxTypes supports an id offset limit.

--- framework/src/Dargon.Vox/src/Dargon.Vox/VoxTypes.cs
+++ framework/src/Dargon.Vox/src/Dargon.Vox/VoxTypes.cs
@@ -5,16 +5,28 @@ namespace Dargon.Vox {
    public abstract class VoxTypes {
       private readonly Dictionary<int, TypeContext> typesById = new Dictionary<int, TypeContext>();
       private readonly int baseId;
+      private readonly int? reservedIdCount;
 
-      protected VoxTypes(int baseId) {
+      /// <param name="baseId">Reserve in VoxTypeIdAssignments.txt</param>
+      /// <param name="reservedIdCount">How many IDs starting at baseId are reserved.</param>
+      protected VoxTypes(int baseId, int? reservedIdCount = null) {
          this.baseId = baseId;
+         this.reservedIdCount = reservedIdCount;
       }
 
       protected void Register<T>(int id) => Register(id, TypeContext.Create<T>());
       protected void Register<T>(int id, Func<T> activator) => Register(id, TypeContext.Create(typeof(T), () => activator()));
       protected void Register(int id, Type type) => Register(id, TypeContext.Create(type));
       protected void Register(int id, Type type, Func<object> activator) => Register(id, TypeContext.Create(type, activator));
-      protected void Register(int id, TypeContext typeContext) => typesById.Add(baseId + id, typeContext);
+      protected void Register(int id, TypeContext typeContext) => typesById.Add(baseId + VerifyIdOffset(id), typeContext);
+
+      private int VerifyIdOffset(int idOffset) {
+         if (reservedIdCount.HasValue && idOffset >= reservedIdCount) {
+            throw new ArgumentOutOfRangeException($"ID Offset is {idOffset} but reservation is in range [{baseId}, {baseId + reservedIdCount.Value}).");
+         }
+
+         return idOffset;
+      }
 
       public IReadOnlyDictionary<int, TypeContext> EnumerateTypes() => typesById;
    }
