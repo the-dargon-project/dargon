623db2876d24dce2cadaf4e9f73ff4b2cb56f5b2
Michael Yu <miyu@dargon.io>
2022-03-02 01:45:38 -0800

    [courier] ApplyDeltaOrThrow in IStateDeltaOperations for convenience

--- framework/src/Dargon.Courier/src/Dargon/Courier/StateReplicationTier/States/IStateDeltaOperations.cs
+++ framework/src/Dargon.Courier/src/Dargon/Courier/StateReplicationTier/States/IStateDeltaOperations.cs
@@ -1,9 +1,15 @@
-﻿namespace Dargon.Courier.StateReplicationTier.States {
+﻿using Dargon.Commons;
+
+namespace Dargon.Courier.StateReplicationTier.States {
    public interface IStateDeltaOperations<TState, TSnapshot, TDelta> : IStateSnapshotOperations<TState, TSnapshot> where TState : IState where TSnapshot : IStateSnapshot where TDelta : IStateDelta {
       /// <summary>
       /// Attempts to apply the delta.
       /// </summary>
       /// <returns>Whether the delta successfully applied. If false, the state must remain unchanged.</returns>
       bool TryApplyDelta(TState state, TDelta delta);
+
+      void ApplyDeltaOrThrow(TState state, TDelta delta) {
+         TryApplyDelta(state, delta).AssertIsTrue();
+      }
    }
 }
