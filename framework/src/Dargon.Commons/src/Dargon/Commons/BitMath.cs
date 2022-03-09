de9477eb11272c5a7d95992f9459b70152b65d3d
Michael Yu <miyu@dargon.io>
2022-03-09 02:56:10 -0800

    [commons] BitMath leverages intrinsics and has tests.

--- framework/src/Dargon.Commons/src/Dargon/Commons/BitMath.cs
+++ framework/src/Dargon.Commons/src/Dargon/Commons/BitMath.cs
@@ -1,27 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Dargon.Commons {
    public static class BitMath {
+      static BitMath() {
+         GetMSB(0U).AssertEquals(0U);
+         GetMSB(1U).AssertEquals(1U);
+         GetMSB(2U).AssertEquals(2U);
+         GetMSB(3U).AssertEquals(2U);
+         GetMSB(4U).AssertEquals(4U);
+         GetMSB(7U).AssertEquals(4U);
+         GetMSB(8U).AssertEquals(8U);
+
+         GetMSBIndex(0U).AssertEquals(-1);
+         GetMSBIndex(1U).AssertEquals(0);
+         GetMSBIndex(2U).AssertEquals(1);
+         GetMSBIndex(3U).AssertEquals(1);
+         GetMSBIndex(4U).AssertEquals(2);
+         GetMSBIndex(5U).AssertEquals(2);
+         GetMSBIndex(7U).AssertEquals(2);
+         GetMSBIndex(8U).AssertEquals(3);
+
+         CeilingPow2(0U).AssertEquals(0U);
+         CeilingPow2(1U).AssertEquals(1U);
+         CeilingPow2(2U).AssertEquals(2U);
+         CeilingPow2(3U).AssertEquals(4U);
+         CeilingPow2(4U).AssertEquals(4U);
+         CeilingPow2(5U).AssertEquals(8U);
+         CeilingPow2(6U).AssertEquals(8U);
+         CeilingPow2(7U).AssertEquals(8U);
+         CeilingPow2(8U).AssertEquals(8U);
+         CeilingPow2(9U).AssertEquals(16U);
+      }
+
       public static uint GetMSB(uint n) {
-         n |= (n >> 1);
-         n |= (n >> 2);
-         n |= (n >> 4);
-         n |= (n >> 8);
-         n |= (n >> 16);
-         return n - (n >> 1);
+         if (n == 0) {
+            return 0;
+         } else {
+            return 1U << BitOperations.Log2(n);
+         }
       }
 
       public static int GetMSBIndex(uint n) {
-         int c = 0;
-         while (n != 0) {
-            c++;
-            n >>= 1;
-         }
-         return c;
+         return n == 0 ? -1 : BitOperations.Log2(n);
       }
 
       public static uint CeilingPow2(uint n) {
