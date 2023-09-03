1d5f2c6a0f5deb4bf4fda2755ed7aabae97550fb
Michael Yu <miyu@dargon.io>
2023-09-03 16:37:24 -0700

    [commons] comment for Unsafe2.IncrementRef

--- framework/src/Dargon.Commons/src/Dargon/Commons/Unsafe2.cs
+++ framework/src/Dargon.Commons/src/Dargon/Commons/Unsafe2.cs
@@ -7,6 +7,14 @@ using System.Threading.Tasks;
 
 namespace Dargon.Commons {
    public static class Unsafe2 {
+      /// <summary>
+      /// Usage (equivalent):
+      ///   x = ref IncrementRef(ref x)
+      /// Note, you cannot do:
+      ///   IncrementRef(ref x) // doesn't change x's address
+      /// </summary>
+      /// <param name="x">reference whose address is to be incremented</param>
+      /// <returns>x</returns>
       [MethodImpl(MethodImplOptions.AggressiveInlining)]
       public static ref T IncrementRef<T>(ref T x) => ref Unsafe.Add<T>(ref x, 1);
    }
